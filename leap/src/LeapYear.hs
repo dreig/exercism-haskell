9afdbf492ebd6fde951ba0a9a837a905154e2408
Andrei Galușca <agalusca@gmail.com>
2020-08-09 19:02:45 +0300

    update solution to leap(Year) using case and pattern-matching

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,10 +1,8 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = if year `mod` 4 /= 0
-                  then False
-                  else if year `mod` 100 /= 0
-                    then True
-                    else if year `mod` 400 == 0
-                      then True
-                      else False
+isLeapYear year = case (year `mod` 4, year `mod` 100, year `mod` 400) of
+  (0, _, 0) -> True
+  (0, 0, _) -> False
+  (0, _, _) -> True
+  (_, _, _) -> False
