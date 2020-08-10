9662bfbaccc64d0eb53109d1eb996f66d6a9c536
Andrei Galușca <agalusca@gmail.com>
2020-08-10 16:56:49 +0300

    redo pangram again using `$` for modifying precedence

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -3,7 +3,4 @@ module Pangram (isPangram) where
 import Data.Char
 
 isPangram :: String -> Bool
-isPangram text =
-  let lowerCaseText = map toLower text
-      inText = flip elem lowerCaseText
-    in all inText ['a'..'z']
+isPangram text = all (flip elem $ map toLower text) ['a'..'z']
