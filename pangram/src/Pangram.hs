8be2bf59e72be0917406b796ea7147520fa3436e
Andrei Galușca <agalusca@gmail.com>
2020-08-10 16:41:06 +0300

    use `flip` and currying instead of defining `inText`

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -5,5 +5,5 @@ import Data.Char
 isPangram :: String -> Bool
 isPangram text =
   let lowerCaseText = map toLower text
-      inText c = c `elem` lowerCaseText
+      inText = flip elem lowerCaseText
     in all inText ['a'..'z']
