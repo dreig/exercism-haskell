2677e36a3dfb759e2da0511e1ff8102709cf7fc8
Andrei Galușca <agalusca@gmail.com>
2020-08-10 14:36:09 +0300

    redo leap using pattern matching

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,8 +1,7 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = case (year `mod` 4, year `mod` 100, year `mod` 400) of
-  (0, _, 0) -> True
-  (0, 0, _) -> False
-  (0, _, _) -> True
-  (_, _, _) -> False
+isLeapYear year
+  | year `mod` 4 == 0
+    = (year `mod` 100 /= 0) || (year `mod` 400 == 0)
+  | otherwise = False
