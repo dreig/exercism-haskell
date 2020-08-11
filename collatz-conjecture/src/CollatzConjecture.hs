ef214acce0c7ee098ac3f8514134260d4bf08a47
Andrei Galușca <agalusca@gmail.com>
2020-08-11 18:52:37 +0300

    redo collatz-conjecture using a recursively defined helper function with an accumulator parameter

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,14 +1,11 @@
 module CollatzConjecture (collatz) where
 
-collatzChain :: (Integral a) => a -> [a]
-collatzChain 1 = []
-collatzChain n
-  | even n  = n : collatzChain (n `div` 2)
-  | odd n   = n : collatzChain (3*n + 1)
-
-
-
 collatz :: Integer -> Maybe Integer
-collatz n
-  | n <= 0    = Nothing
-  | otherwise = Just (fromIntegral $ length (collatzChain n))
+collatz num
+  | num <= 0 = Nothing
+  | otherwise = Just (step num 0)
+    where
+  step n acc
+    | n == 1 = acc
+    | odd n  = step (3*n+1) (acc + 1)
+    | even n = step (n `div` 2) (acc + 1)
