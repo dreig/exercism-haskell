eff115e039784ace415781259b70d1da818bfe85
Andrei Galușca <agalusca@gmail.com>
2020-08-11 19:05:30 +0300

    redo collatz-conjecture non-recursively using `iterate`

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,9 +3,8 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz num
   | num <= 0 = Nothing
-  | otherwise = Just (step num 0)
-    where
-  step n acc
-    | n == 1 = acc
-    | odd n  = step (3*n+1) (acc + 1)
-    | even n = step (n `div` 2) (acc + 1)
+  | otherwise = Just (fromIntegral $ length $ takeWhile (/=1) (iterate collatzStep num))
+  where collatzStep 1 = 1
+        collatzStep n
+          | odd n = 3*n + 1
+          | even n = n `div` 2
