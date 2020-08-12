7c588906ab52e06033264da28c0b8d9f52ef6491
Andrei Galușca <agalusca@gmail.com>
2020-08-12 16:56:55 +0300

    redo sum-of-multiples by prebuilding the lists of multiples for each factor

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,6 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.List (nub)
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum multiples
-  where multiples = [ c | c <- [1..(limit-1)], any ((==0) . (c `mod`)) nonZeroFactors]
-        nonZeroFactors = filter (/=0) factors
+sumOfMultiples factors limit = sum $ nub $ concat [limitedMultiplesOf factor | factor <- factors]
+  where multiplesOf 0 = []
+        multiplesOf factor = scanl1 (+) (repeat factor)
+        limitedMultiplesOf = takeWhile (<limit) . multiplesOf
