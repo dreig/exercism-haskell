d0bc128cb5b7bf13fbeeaef95db237d34492ca8c
Andrei Galușca <agalusca@gmail.com>
2020-08-15 03:25:20 +0300

    update solution to nucleotide-count to build the required Map by using foldM over a map of Either-values

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -4,8 +4,10 @@ module DNA (
 ) where
 
 import qualified Data.Map as M
+import Control.Monad (foldM)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
+type NucleotideCount = M.Map Nucleotide Int
 
 parseNucleotide :: Char -> Either String Nucleotide
 parseNucleotide c = case c of
@@ -15,9 +17,26 @@ parseNucleotide c = case c of
   'T' -> Right T
   _ -> Left $ "Char " ++ show c ++ " is not a valid Nucleotide."
 
-nucleotideCounts :: String -> Either String (M.Map Nucleotide Int)
-nucleotideCounts xs = case traverse parseNucleotide xs of
-  (Left err) -> Left err
-  (Right nucleotides) -> Right $ M.fromListWith (+) $ zip nucleotides (repeat 1)
+initialMap :: NucleotideCount
+initialMap = M.fromList
+  [(A,0)
+  ,(C,0)
+  ,(G,0)
+  ,(T,0)
+  ]
 
+nucleotideCounts :: String -> Either String NucleotideCount
+nucleotideCounts xs = foldM insertM initialMap $ map parseNucleotide xs
+  where insertM = fmap . flip (M.adjust (+1))
+  {-
+    `insertM` takes two arguments, the accumulator Map and the Either-wrapped Nucleotide
+              and returns the updated accumulator Map (if the Either-wrapped Nucleotide is a Right-value)
 
+    :type insertM
+    :: (Functor f, Ord a1, Num a2) =>
+        M.Map a1 a2 -> f a1 -> f (M.Map a1 a2)
+
+    we have to `flip` (M.adjust (+1)) since its arguments are reversed from what `foldM` expects
+    :type M.adjust (+1)
+    :: (Ord k, Num a) => k -> M.Map k a -> M.Map k a
+  -}
