1daddd07b406b3075369a7af383c22e1ce8a8fd0
Andrei Galușca <agalusca@gmail.com>
2020-08-15 15:58:08 +0300

    simplify solution to `rna-transcription` using `traverse`

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,17 +1,12 @@
 module DNA (toRNA) where
 
--- TODO: use fmap to naturally combine / process the Either type
 toRNA :: String -> Either Char String
-toRNA str
-  | not (null firstInvalidChar) = Left (head firstInvalidChar)
-  where firstInvalidChar = take 1 (dropWhile (`elem` "CGTA") str)
-toRNA str = Right (validToRNA str)
+toRNA = traverse toRNABase
 
-validToRNA :: String -> String
-validToRNA "" = ""
-validToRNA (x:xs)
-  | x == 'G' = 'C' : validToRNA xs
-  | x == 'C' = 'G' : validToRNA xs
-  | x == 'T' = 'A' : validToRNA xs
-  | x == 'A' = 'U' : validToRNA xs
-  | otherwise = error "this should never happen"
+toRNABase :: Char -> Either Char Char
+toRNABase c = case c of
+  'G' -> Right 'C'
+  'C' -> Right 'G'
+  'T' -> Right 'A'
+  'A' -> Right 'U'
+  _   -> Left c
