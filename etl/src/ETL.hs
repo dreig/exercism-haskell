e7e6b446f8adde9a1394eb604bb946fc593b6b8c
Andrei Galușca <agalusca@gmail.com>
2020-08-17 02:46:30 +0300

    update solution to etl: use Map.fromList instead of constructing the Map by hand

--- etl/src/ETL.hs
+++ etl/src/ETL.hs
@@ -4,6 +4,5 @@ import Data.Char (toLower)
 import qualified Data.Map as M
 
 transform :: M.Map a String -> M.Map Char a
-transform legacyData = foldr (uncurry M.insert) M.empty letterPts
-  where letterPts = [(toLower letter, pts) | (pts, letters) <- M.toList legacyData, letter <- letters]
+transform legacyData = M.fromList [(toLower letter, pts) | (pts, letters) <- M.toList legacyData, letter <- letters]
 
