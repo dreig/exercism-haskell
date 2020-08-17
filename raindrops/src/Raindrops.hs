33b77d8c0974c20d6b22a2d87697b9584ea7a692
Andrei Galușca <agalusca@gmail.com>
2020-08-17 07:47:14 +0300

    update solution to raindrops

--- raindrops/src/Raindrops.hs
+++ raindrops/src/Raindrops.hs
@@ -2,10 +2,8 @@ module Raindrops (convert) where
 
 convert :: Int -> String
 convert n =
-  if null sound
+  if null phrase
     then show n
-    else sound
-  where pling = if n `mod` 3 == 0 then "Pling" else ""
-        plang = if n `mod` 5 == 0 then "Plang" else ""
-        plong = if n `mod` 7 == 0 then "Plong" else ""
-        sound = pling ++ plang ++ plong
+    else phrase
+  where numberSounds = zip [3,5,7] ["Pling", "Plang", "Plong"]
+        phrase = concatMap snd $ filter ((==0) . (n `mod`) . fst) numberSounds
