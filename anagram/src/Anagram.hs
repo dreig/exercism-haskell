0c0b91116c4888ff854824166f6aeb4171812470
Andrei Galușca <agalusca@gmail.com>
2020-08-17 11:36:32 +0300

    update solution to anagrams: use `on` to compare a function on two values

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -2,9 +2,11 @@ module Anagram (anagramsFor) where
 
 import Data.Char (toLower)
 import Data.List (sort)
+import Data.Function (on)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = filter ((/= map toLower xs) . map toLower) possibleMatches
-  where possibleMatches = filter ((== canonicalOriginal) . canonicalRep) xss
-        canonicalRep = sort . map toLower
-        canonicalOriginal = canonicalRep xs
+anagramsFor xs xss = filter (distinctFrom xs) $ filter (anagramOf xs) xss
+  where anagramOf = (==) `on` (sort . map toLower)
+        distinctFrom = (/=) `on` map toLower
+
+
