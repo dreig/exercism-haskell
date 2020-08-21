8c827d4a813119a744614d3a7a6ed72873b8af48
Andrei Galușca <agalusca@gmail.com>
2020-08-22 01:46:06 +0300

    simplify solution to prime-factors
    though the prime-sieve is still slow/inefficient

--- prime-factors/src/PrimeFactors.hs
+++ prime-factors/src/PrimeFactors.hs
@@ -1,7 +1,5 @@
 module PrimeFactors (primeFactors) where
 
-import Data.List(span, filter)
-
 primes :: [Integer]
 primes = filterPrimes (2:[3,5..])
   where filterPrimes (p:xs) = p: filterPrimes (filter ((/=0) . (`mod` p)) xs)
@@ -9,16 +7,7 @@ primes = filterPrimes (2:[3,5..])
 primeFactors :: Integer -> [Integer]
 primeFactors n = go n primes
   where go 1 _ = []
-        go x primes' =
-          let (_, (factor : tailPrimes)) = span (\p -> p*p <= x && x `mod` p /= 0) primes'
-          in (
-            if factor * factor > x
-              then [x]
-              else let (factors, x')  = repeatFactor factor x []
-                   in (factors ++ (go x' tailPrimes))
-          )
-
-repeatFactor :: Integer -> Integer -> [Integer] -> ([Integer], Integer)
-repeatFactor factor n xs
-  | n `mod` factor /= 0 = (xs, n)
-  | otherwise = repeatFactor factor (n `div` factor) (factor : xs)
+        go x (p:xs)
+          | p * p > x = [x]
+          | x `mod` p == 0 = p : go (x `div` p) (p:xs)
+          | otherwise = go x xs
