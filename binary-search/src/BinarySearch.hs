7f33878f2dfdda9b5016ec123991bcd12a4049e1
Andrei Galușca <agalusca@gmail.com>
2020-09-04 00:13:50 +0300

    rewrite solution to binary-search to use guards instead of nested if-else-then

--- binary-search/src/BinarySearch.hs
+++ binary-search/src/BinarySearch.hs
@@ -4,11 +4,10 @@ import Data.Array
 
 find :: Ord a => Array Int a -> a -> Maybe Int
 find arr x
-  | null arr = Nothing
-  | otherwise = let (l,r) = bounds arr
-                    m = l + (r-l) `div` 2
-                in if arr ! m == x
-                      then Just m
-                      else if arr ! m > x
-                              then find (ixmap (l, m-1) id arr) x
-                              else find (ixmap (m+1, r) id arr) x
+    | null arr      = Nothing
+    | arr ! m == x  = Just m
+    | arr ! m > x   = find (ixmap (l, m-1) id arr) x
+    | otherwise     = find (ixmap (m+1, r) id arr) x
+    where
+        (l, r) = bounds arr
+        m      = l + (r-l) `div` 2
