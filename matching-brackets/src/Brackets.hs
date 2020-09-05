a1c8f34efd46905f20a7bc723039dd983e0e9510
Andrei Galușca <agalusca@gmail.com>
2020-09-05 16:39:08 +0300

    update solution to matching-brackets

--- matching-brackets/src/Brackets.hs
+++ matching-brackets/src/Brackets.hs
@@ -1,17 +1,17 @@
 module Brackets (arePaired) where
 
+import Data.List (foldl')
+
 isBracket :: Char -> Bool
 isBracket = (`elem` "(){}[]")
 
+matchingBrackets :: (Char, Char) -> Bool
+matchingBrackets = (`elem` zip "({[" ")}]")
+
+-- inspired from https://exercism.io/tracks/haskell/exercises/matching-brackets/solutions/4e50bef28aa9420991df7d2130a1d843
 arePaired :: String -> Bool
-arePaired = go [] . filter isBracket
-  where
-    go stack "" = null stack
-    go arr (x:xs)
-      | openingBracket x     = go (x:arr) xs
-      | null arr             = False
-    go (y:ys) (x:xs)
-      | matchingBrackets y x = go ys xs
-      | otherwise            = False
-    openingBracket = (`elem` "({[")
-    matchingBrackets = curry (`elem` zip "({[" ")}]")
+arePaired = null . foldl' removeMatching [] . filter isBracket
+            where
+              removeMatching stack x
+                | (top:stack') <- stack, matchingBrackets (top,x) = stack'
+                | otherwise                                       = x : stack
