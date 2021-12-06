975627f3f4ca9979f0ff8b0e23fec779bffcbd13
Andrei Galușca <agalusca@gmail.com>
2021-12-06 18:57:54 +0200

    solve Leap

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,7 +1,8 @@
 module LeapYear (isLeapYear) where
 
-isLeapYear :: Integer -> Bool
-isLeapYear year
-  | year `mod` 4 == 0
-    = (year `mod` 100 /= 0) || (year `mod` 400 == 0)
-  | otherwise = False
+isLeapYear :: Int -> Bool
+isLeapYear n
+  | n `mod` 400 == 0 = True
+  | n `mod` 100 == 0 = False
+  | n `mod` 4   == 0 = True
+  | otherwise        = False
